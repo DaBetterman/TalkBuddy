e60a0cc52ad8215d3fafa8b1bdf6c4170283e7ef
DaBetterman <1ebrahimr@gmail.com>
2024-09-15 17:24:00 +0200

    Added render.yaml and requirement list

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def calendar():
     return render_template('calendar.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
+
